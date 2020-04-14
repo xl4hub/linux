8a8b5c9534cc2ed6b9d1cff4e87640f11d7d51ad
Marc Kleine-Budde <mkl@pengutronix.de>
2020-04-14 11:00:21 +0200

    can: mcp25xxfd: quirks: let the priv hold a copy of the quirks, so that it can be extended by DT properties
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp25xxfd/mcp25xxfd-regmap.c
+++ drivers/net/can/spi/mcp25xxfd/mcp25xxfd-regmap.c
@@ -367,7 +367,7 @@ mcp25xxfd_regmap_init_nocrc(struct mcp25xxfd_priv *priv)
 static int
 mcp25xxfd_regmap_init_crc(struct mcp25xxfd_priv *priv)
 {
-	if (!(priv->devtype_data->quirks & MCP25XXFD_QUIRK_RX_CRC)) {
+	if (!(priv->devtype_data.quirks & MCP25XXFD_QUIRK_RX_CRC)) {
 		priv->map_rx = priv->map;
 
 		return 0;
